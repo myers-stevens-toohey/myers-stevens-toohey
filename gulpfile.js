1c91cc7ba989310b7f01d1314e707cf442a163c4
joshuawarnock <joshua.warnock@gmail.com>
2015-11-19 13:04:34 -0800

    Completed minify-html, minify-css, uglify. The results were piped to public

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,12 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
 var jshint = require('gulp-jshint');
+var minHtml = require('gulp-minify-html');
+var minCss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
 
 gulp.task('watch', function() {
-   gulp.watch('/src/**', ['jshint', 'copy-src']);
+   gulp.watch('/src/**', ['jshint', 'htmlmin']);
 });
 
 gulp.task('start', function() {
@@ -16,15 +19,32 @@ gulp.task('start', function() {
    })
 });
 
-gulp.task('copy-src', function() {
-    gulp.src('./src/**')
-        .pipe(gulp.dest('./public'));
+gulp.task('minify-html', function() {
+    var opts = {
+        conditionals:true,
+        spare:true
+    };
+
+    return gulp.src('./src/*.html')
+        .pipe(minHtml(opts))
+        .pipe(gulp.dest('./public/'));
+});
+
+gulp.task('minify-css', function() {
+    return gulp.src('./src/css/*.css')
+        .pipe(minCss({compatibility: 'ie8'}))
+        .pipe(gulp.dest('./public/css/'))
+});
+
+gulp.task('uglify', function() {
+    return gulp.src('./src/js/*.js')
+        .pipe(gulp.dest('./public/js/'))
 });
 
 gulp.task('jshint', function() {
-    return gulp.src('src/js/**')
+    return gulp.src('src/js/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'));
 });
 
-gulp.task('default', ['start', 'watch', 'copy-src', 'jshint']);
+gulp.task('default', ['jshint', 'minify-html', 'minify-css', 'uglify', 'start', 'watch']);
