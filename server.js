e166f038c17c05bf7f95da3c239841608140237b
Jwarnock <jwarnock@Jwarnocks-MacBook-Pro.local>
2015-11-19 10:18:58 -0800

    Added server.js on port 3000 and static path for public dir

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 /**
  * Created by jwarnock on 11/19/15.
  */
+var express = require('express');
+var app = express();
+
+app.use(function(req, res, next) {
+    console.log(req.url);
+    next()
+});
+
+app.use(express.static('public'));
+
+var server = app.listen(3000, function() {
+    console.log('Server is running at http://localhost:' +
+    server.address().port);
+});
